5feeda94550ce24e89c2a7b8ae69a2d676a3e2a6
Deepansha Tiwari <Deepansha.Tiwari@target.com>
2019-06-22 13:57:57 -0500

    completed the homepage

--- fotography/src/App.js
+++ fotography/src/App.js
@@ -3,12 +3,18 @@ import logoPt from './logoPt.png';
 import homeImage from './homeImage.JPG';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile'
+import ListSubheader from '@material-ui/core/ListSubheader'
+import IMG_0201 from './IMG_0201.jpg'
+import IMG_0299 from './IMG_0299.JPG'
+import IMG_6185 from './IMG_6185.JPG'
+import IMG_0132 from './IMG_0132.JPG'
+import IMG_2956 from './IMG_2956.jpg'
+import temp from './temp.png'
+import IMG_3666 from './IMG_3666.JPG'
 import './App.css';
 import ButtonAppBar from './ButtonAppBar'
-
-
 
 const styles = {
   image: {
@@ -20,11 +26,10 @@ const styles = {
     flexWrap: 'wrap',
     justifyContent: 'space-around',
     overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
+    backgroundColor: '#A07GHT',
   },
   gridList: {
-    width: 500,
-    height: 450,
+    
   },
   icon: {
     color: 'rgba(255, 255, 255, 0.54)',
@@ -33,32 +38,51 @@ const styles = {
 function App(props) {
   
   const { classes } = props;
-
+  const tile = {
+    author: 'Parth Tiwari'
+  }
   return (
     <div className="App">
       <ButtonAppBar />
 
-
-/**
-This needs to change  */
-      <GridList cellHeight={180} className={classes.gridList}>
+      <GridList cellHeight={450} className={classes.gridList}>
         <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-          <ListSubheader component="div">December</ListSubheader>
+          <ListSubheader style= {{backgroundColor: '#d1460a'}}>.</ListSubheader>
         </GridListTile>
-        {tileData.map(tile => (
-          <GridListTile key={tile.img}>
-            <img src={tile.img} alt={tile.title} />
-            <GridListTileBar
-              title={tile.title}
-              subtitle={<span>by: {tile.author}</span>}
-              actionIcon={
-                <IconButton className={classes.icon}>
-                  <InfoIcon />
-                </IconButton>
-              }
-            />
+        
+          <GridListTile style= {{width: '710px'}}>  
+            <img src={IMG_0299}/>
+          
           </GridListTile>
-        ))}
+          <GridListTile style= {{width: '870px'}}> 
+            <img src={IMG_0201} />
+          
+          </GridListTile>
+          <GridListTile style= {{width: '870px'}}> 
+            <img src={IMG_3666} />
+          
+          </GridListTile>
+          <GridListTile style= {{width: '710px'}}> 
+            <img src={IMG_6185} />
+          
+          </GridListTile>
+
+          <GridListTile style= {{width: '710px'}}>  
+            <img src={IMG_0132}/>
+          
+          </GridListTile>
+          <GridListTile style= {{width: '870px'}}> 
+            <img src={IMG_2956} />
+          
+          </GridListTile>
+          <GridListTile style= {{width: '870px'}}> 
+            <img src={temp} />
+          
+          </GridListTile>
+          <GridListTile style= {{width: '710px'}}> 
+            <img src={homeImage} />
+          
+          </GridListTile> 
       </GridList>
       
       {/* <Button variant="outlined" color="#ffffff" className={classes.button}>
